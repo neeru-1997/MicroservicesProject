0f7ed0c37a7c58f34cd11cf4a263dae45efe4dec
Neeraj <Neeraj@github.com>
2022-04-13 23:31:33 +0530

    Using RestTemplate for communication.

--- customer/src/main/java/com/majka/customer/CustomerService.java
+++ customer/src/main/java/com/majka/customer/CustomerService.java
@@ -2,12 +2,14 @@ package com.majka.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -19,9 +21,21 @@ public class CustomerService {
         //check if email valid
 
         //check if email not taken
+        customerRepository.saveAndFlush(customer);
+
+        //check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class, customer.getId());
+
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Fraudster");
+        }
 
         // save customer to db
-        customerRepository.save(customer);
+//        customerRepository.save(customer);
+
+        //send notification
     }
 
 }
--- fraud/src/main/java/com/majka/fraud/FraudController.java
+++ fraud/src/main/java/com/majka/fraud/FraudController.java
@@ -1,6 +1,7 @@
 package com.majka.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -16,6 +18,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID){
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("fraud check request for customer {}", customerID);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 
