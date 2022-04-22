1199f41339428d462fd1fd2b8acbbd02e5b2958b
Neeraj <Neeraj@github.com>
2022-04-22 18:25:18 +0530

    Publishing Messages

--- notification/src/main/java/com/majka/notification/NotificationApplication.java
+++ notification/src/main/java/com/majka/notification/NotificationApplication.java
@@ -1,13 +1,31 @@
 package com.majka.notification;
 
+import com.majka.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.majka.notification",
+                "com.majka.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+                                        NotificationConfig notificationConfig){
+        return args -> {
+            producer.publish("foo",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
     }
 }
